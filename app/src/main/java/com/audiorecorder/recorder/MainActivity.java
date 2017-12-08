37bfe47ea34416bd1fceaf0641c0e7874367f909
LoicNigra <loic_nigra@hotmail.com>
2017-12-08 01:52:01 +0100

    Aparte klasses

--- app/src/main/java/com/audiorecorder/recorder/MainActivity.java
+++ app/src/main/java/com/audiorecorder/recorder/MainActivity.java
@@ -10,21 +10,21 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+// import static com.audiorecorder.recorder.Permissies.*;
+import static com.audiorecorder.recorder.Opnemen.*;
+import static com.audiorecorder.recorder.Stoppen.*;
+import static com.audiorecorder.recorder.Afspelen.*;
+import static com.audiorecorder.recorder.Variabelen.*;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MainActivity extends AppCompatActivity {
-
-    private MediaPlayer mediaPlayer;
-    private MediaRecorder mediaRecorder;
-    private static int teller = 0;
-    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 3;
-    private String directory = Environment.getExternalStorageDirectory().getPath();
-    private String fileName = "/audio" + teller + ".3gpp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,41 +32,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Permissies();
 
-
     }
 
     public void Klik(View view) {
         switch (view.getId()) {
             case R.id.opnemen:
                 try {
-
-                    Opnemen();
+                    opnemen();
+                    Toast.makeText(getApplicationContext(), "Bezig met opnemen van opname " + teller, Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
-                    //    Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden met het opnemen", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden met het opnemen", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
                 break;
             case R.id.stoppen:
                 try {
-                    Stoppen();
+                    OpnemenStoppen();
+                    Toast.makeText(getApplicationContext(), "Opname is gestopt van opname " + teller, Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
-                    //  Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden het stoppen", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden het stoppen", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
                 break;
             case R.id.afspelen:
                 try {
-                    Afspelen();
+
+                    afspelen();
+                    Toast.makeText(getApplicationContext(), "Opname " + teller + " is aan het afspelen", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
-                    //  Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden het afspelen", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
                 break;
             case R.id.stopAfspelen:
                 try {
                     AfspelenStoppen();
+                    Toast.makeText(getApplicationContext(), "Het afspelen van opname " + teller + " is gestopt", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
-                    // Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden het afspelen te stoppen", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden het afspelen te stoppen", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
                 break;
@@ -74,90 +76,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void Opnemen() throws Exception {
 
+    public boolean Permissies() {
 
-        // Moest er al een recorder opstaan, stoppen we die
-        StopMediarecorder();
-
-    File outFile = new File(directory, fileName);
-    while(outFile.exists()){
-
-        teller++;
-        fileName = "/audio" + teller + ".3gpp";
-        outFile = new File(directory, fileName);
-    }
-
-
-        // Mediarecorder opzetten
-        mediaRecorder = new MediaRecorder();
-        // De source opzetten van waar de audio in komt => Dit is de microfoon
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        // De extension
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-
-        // De encoder => Later dan versie 2.3.3, kan AMR_WB => Voor compatibilty doen we de NB, NarrowBand
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        // Waar opslaan
-     mediaRecorder.setOutputFile(directory + fileName);
-
-        // Klaarzetten
-        mediaRecorder.prepare();
-        //Starten
-        mediaRecorder.start();
-
-    }
-
-
-    private void Stoppen() {
-        // als mediaRecorder bezig is, stoppen we deze
-        if(mediaRecorder != null) {
-            mediaRecorder.stop();
-        }
-    }
-
-
-    private void Afspelen() throws IOException {
-
-
-        // Checken als er recorder aanstaat, zoja dan stoppen
-        StopMediaPlayer();
-
-        mediaPlayer = new MediaPlayer();
-        // De gegevens van de outputfile nemen
-        mediaPlayer.setDataSource(directory + fileName);
-        // mediaPlayer Klaarzetten
-        mediaPlayer.prepare();
-        //media afspelen
-        mediaPlayer.start();
-    }
-
-    private void AfspelenStoppen() {
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-        }
-    }
-
-    private void StopMediarecorder() {
-        if (mediaRecorder != null) {
-            mediaRecorder.release();
-        }
-    }
-
-
-    private void StopMediaPlayer() {
-        if (mediaPlayer != null) {
-            try {
-                mediaPlayer.release();
-            } catch (Exception e) {
-                // Toast.makeText(getApplicationContext(), "Er is een probleem opgetreden.", Toast.LENGTH_LONG).show();
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    private boolean Permissies() {
         int permissionRecord = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
         int permissionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         int permissionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
@@ -180,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
 
 
 
