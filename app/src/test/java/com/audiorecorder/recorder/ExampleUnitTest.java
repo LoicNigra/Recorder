3ce7e7939c22000a9bd1216d59c1009910496388
LoicNigra <loic_nigra@hotmail.com>
2017-12-22 01:13:30 +0100

    Unit test + Onnodige code verwijdert in XML bestanden

--- app/src/test/java/com/audiorecorder/recorder/ExampleUnitTest.java
+++ app/src/test/java/com/audiorecorder/recorder/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.audiorecorder.recorder;
 
 import org.junit.Test;
 
+import static com.audiorecorder.recorder.Methodes.Opnemen.*;
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +14,16 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void timer_isCorrect() throws Exception {
+        seconds = 60;
+        minutes = 0;
+
+        if (seconds == 60){
+            assertEquals(60,0);
+        }
+
     }
 }
